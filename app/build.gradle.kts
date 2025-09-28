8856bee0baa1472bc0ecdfadcdbe0f14ccdfddaf
Rohit Sharma <rohitksharma345@gmail.com>
2025-09-28 20:48:50 +0530

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.dagger.hilt.android")
     kotlin("kapt")
 }
 
@@ -49,6 +48,4 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
 
     implementation("com.google.dagger:hilt-android:2.57.1")
-    kapt("com.google.dagger:hilt-compiler:2.57.1")
-
 }
